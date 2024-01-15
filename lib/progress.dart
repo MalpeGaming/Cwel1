b495c266b588402a20618fae076109313aead97a
mzums <annsie035@gmail.com>
2024-01-15 14:49:12 +0100

    built charts, for now without reading from xml

--- lib/progress.dart
+++ lib/progress.dart
@@ -10,6 +10,68 @@ class Progress extends StatefulWidget {
 }
 
 class _Progress extends State<Progress> {
+  SizedBox buildChart(BuildContext context, int dayNum) {
+    Size size = MediaQuery.of(context).size;
+    return SizedBox(
+      width: 0.13 * size.width,
+      height: 0.13 * size.width,
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Text(
+            dayNum.toString(),
+            style: TextStyle(fontSize: 0.05 * size.width),
+          ),
+          PieChart(
+            PieChartData(
+              startDegreeOffset: 270,
+              sections: [
+                PieChartSectionData(
+                  gradient: const LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment(0.8, 1),
+                    colors: <Color>[
+                      Color.fromARGB(255, 0, 2, 122),
+                      Color.fromARGB(255, 27, 126, 255),
+                    ],
+                    tileMode: TileMode.mirror,
+                  ),
+                  radius: 8,
+                  value: 20,
+                  title: '',
+                  color: Theme.of(context).colorScheme.onPrimary,
+                ),
+                PieChartSectionData(
+                  radius: 8,
+                  value: 80,
+                  title: '',
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+              ],
+            ),
+            swapAnimationDuration: const Duration(milliseconds: 1000),
+            swapAnimationCurve: Curves.linear,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Column buildChartRow(BuildContext context, int i) {
+    Size size = MediaQuery.of(context).size;
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            for (int j = 1; j <= 5; ++j) buildChart(context, (i - 1) * 5 + j),
+          ],
+        ),
+        SizedBox(height: 0.03 * size.height),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -46,49 +108,11 @@ class _Progress extends State<Progress> {
               ],
             ),
             SizedBox(height: 0.05 * size.height),
-            SizedBox(
-              width: 0.15 * size.width,
-              height: 0.15 * size.width,
-              child: Stack(
-                alignment: Alignment.center,
-                children: [
-                  Text(
-                    "10",
-                    style: TextStyle(fontSize: 0.05 * size.width),
-                  ),
-                  PieChart(
-                    PieChartData(
-                      startDegreeOffset: 270,
-                      sections: [
-                        PieChartSectionData(
-                          gradient: const LinearGradient(
-                            begin: Alignment.topLeft,
-                            end: Alignment(0.8, 1),
-                            colors: <Color>[
-                              Color.fromARGB(255, 0, 2, 122),
-                              Color.fromARGB(255, 27, 126, 255),
-                            ],
-                            tileMode: TileMode.mirror,
-                          ),
-                          radius: 10,
-                          value: 20,
-                          title: '',
-                          color: Theme.of(context).colorScheme.onPrimary,
-                        ),
-                        PieChartSectionData(
-                          radius: 10,
-                          value: 80,
-                          title: '',
-                          color: Theme.of(context).colorScheme.secondary,
-                        ),
-                      ],
-                    ),
-                    swapAnimationDuration: const Duration(milliseconds: 1000),
-                    swapAnimationCurve: Curves.linear,
-                  ),
-                ],
-              ),
-            ),
+            Column(
+              children: [
+                for (int i = 1; i <= 6; ++i) buildChartRow(context, i)
+              ],
+            )
           ],
         ),
       ),
