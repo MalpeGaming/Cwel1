8536abecb43be0869a32808a0e9f642b54b8199a
piter231 <piotrgajak02@gmail.com>
2024-02-27 22:16:44 +0100

    added an introduction page into 'logical thinking'

---
+++ lib/logical_thinking/logical_thinking.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+import '../buttons.dart';
+import '../improvement_selection.dart';
+
+class LogicalThinking extends StatefulWidget {
+  final bool? initialTest;
+  const LogicalThinking({this.initialTest = false, super.key});
+
+  @override
+  State<LogicalThinking> createState() => _LogicalThinking();
+}
+
+class _LogicalThinking extends State<LogicalThinking> {
+  bool initialTest = false;
+
+  @override
+  void initState() {
+    super.initState();
+    initialTest = widget.initialTest!;
+    print(initialTest);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(height: 0.05 * size.height),
+          Align(
+            alignment: Alignment.center,
+            child: Column(
+              children: [
+                Text(
+                  "LOGICAL",
+                  style: TextStyle(fontSize: 0.08 * size.height),
+                ),
+                Text(
+                  "THINKING",
+                  style: TextStyle(fontSize: 0.035 * size.height),
+                ),
+                Text(
+                  "Exercise 1 - Math practice",
+                  style: TextStyle(fontSize: 0.03 * size.height),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: RichText(
+                    text: TextSpan(
+                      style: TextStyle(
+                        fontSize: 0.022 * size.height,
+                        color: Theme.of(context).colorScheme.onSecondary,
+                      ),
+                      children: const [
+                        TextSpan(
+                          text:
+                              "In this exercises you will complete part of the ",
+                        ),
+                        TextSpan(
+                          text: "SAT Math with CALCULATOR",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        TextSpan(
+                          text: " Part.",
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 0.015 * size.height),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: RichText(
+                    text: TextSpan(
+                      style: TextStyle(
+                        fontSize: 0.022 * size.height,
+                        color: Theme.of(context).colorScheme.onSecondary,
+                      ),
+                      children: const [
+                        TextSpan(
+                          text: "You will have ",
+                        ),
+                        TextSpan(
+                          text: "350 seconds",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        TextSpan(
+                          text: ".",
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 0.015 * size.height),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: RichText(
+                    text: TextSpan(
+                      style: TextStyle(
+                        fontSize: 0.022 * size.height,
+                        color: Theme.of(context).colorScheme.onSecondary,
+                      ),
+                      children: const [
+                        TextSpan(
+                          text: "When ready click \" ",
+                        ),
+                        TextSpan(
+                          text: "CONTINUE",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        TextSpan(
+                          text: ".\"",
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Spacer(),
+          Center(
+            child: SizedBox(
+              height: size.height * 0.05,
+              width: size.width * 0.75,
+              child: RedirectButton(
+                route: const ImprovementSelection(),
+                text: 'Continue',
+                width: size.width,
+              ),
+            ),
+          ),
+          SizedBox(height: 0.1 * size.height),
+        ],
+      ),
+    );
+  }
+}
