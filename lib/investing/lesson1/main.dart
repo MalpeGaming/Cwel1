35f4c406fe834c3a542b2d1393576ad5223989ed
mzums <annsie035@gmail.com>
2024-06-08 12:16:06 +0200

    investing - lesson1 done

--- lib/investing/lesson1/main.dart
+++ lib/investing/lesson1/main.dart
@@ -12,12 +12,33 @@ class Lesson1 extends StatefulWidget {
 class _Lesson1 extends State<Lesson1> {
   int selectedOption = -1;
 
+  Widget keyVocabulary(String text, String definition) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          text,
+          style: TextStyle(
+            fontSize: MediaQuery.of(context).size.height * 0.020,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          definition,
+          style: TextStyle(
+            fontSize: MediaQuery.of(context).size.height * 0.020,
+          ),
+        ),
+      ],
+    );
+  }
+
   Widget buildQuizScreen({
-    required String question,
-    required List<String> answers,
-    required int correctAnswer,
+    required int questionNumber,
     String? image,
   }) {
+    List<String> answers = questions[questionNumber]["answers"] as List<String>;
+    Size size = MediaQuery.of(context).size;
     ListTile createListTitle(int val, String text, Size size) {
       return ListTile(
         dense: true,
@@ -39,9 +60,17 @@ class _Lesson1 extends State<Lesson1> {
     }
 
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
-          question,
+          "Exercise ${questionNumber + 1}.",
+          style: TextStyle(
+            fontWeight: FontWeight.w900,
+            fontSize: 0.02 * size.height,
+          ),
+        ),
+        Text(
+          questions[questionNumber]["question"] as String,
           style:
               TextStyle(fontSize: MediaQuery.of(context).size.height * 0.020),
         ),
@@ -50,18 +79,54 @@ class _Lesson1 extends State<Lesson1> {
         Column(
           children: List.generate(answers.length, (index) {
             return Container(
-              //margin: const EdgeInsets.all(5),
-              child: createListTitle(
-                index,
-                answers[index],
-                MediaQuery.of(context).size,
-              ),
+              child: createListTitle(index, answers[index], size),
             );
           }),
         ),
       ],
     );
   }
+
+  var questions = [
+    {
+      "question":
+          "Below you can see a chart showing the inflation rate in Algeria during a time period of 4 years. Let’s assume an individual holds \$100k in cash at the beginning of the year 2019. How much will his money be worth at the end of the year 2022?",
+      "answers": [
+        "around \$80.5k",
+        "around \$90k",
+        "around \$85k",
+        "around \$92k",
+      ],
+      "correctAnswer": 1,
+    },
+    {
+      "question":
+          "Buying a house which generates maintenance costs, means it is a/an:",
+      "answers": [
+        "asset",
+        "liability",
+      ],
+      "correctAnswer": 1,
+    },
+    {
+      "question": "Assets generate income … if you work.",
+      "answers": [
+        "only",
+        "regardless",
+      ],
+      "correctAnswer": 1,
+    },
+    {
+      "question": "Because of inflation our cash savings … in value.",
+      "answers": [
+        "increase",
+        "decrease",
+      ],
+      "correctAnswer": 1,
+    },
+  ];
+
+  List<int> usersAnswers = List<int>.filled(4, -1);
 
   @override
   Widget build(BuildContext context) {
@@ -83,9 +148,10 @@ class _Lesson1 extends State<Lesson1> {
                 "Lesson 1",
                 style: TextStyle(
                   fontSize: size.width / 10,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-              SizedBox(height: size.height / 80),
+              SizedBox(height: size.height / 60),
               Text(
                 "Why Should You Invest?",
                 style: TextStyle(
@@ -99,7 +165,7 @@ class _Lesson1 extends State<Lesson1> {
                   fontSize: size.height / 50,
                 ),
               ),
-              SizedBox(height: size.height / 60),
+              SizedBox(height: size.height / 20),
               Text(
                 "1. Beat the Inflation",
                 style: TextStyle(
@@ -110,49 +176,18 @@ class _Lesson1 extends State<Lesson1> {
               SizedBox(height: size.height / 40),
               Text(
                 "Key vocabulary:",
-                style: TextStyle(fontSize: size.height * 0.020),
-              ),
-              SizedBox(height: size.height / 60),
-              RichText(
-                text: TextSpan(
-                  style: TextStyle(
-                    fontSize: 0.020 * size.height,
-                    color: Theme.of(context).colorScheme.onSecondary,
-                  ),
-                  children: const [
-                    TextSpan(
-                      text: "investing",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    TextSpan(
-                      text:
-                          "- put (money) into financial schemes, shares, property, or a commercial venture with the expectation of achieving a profit ",
-                    ),
-                    TextSpan(
-                      text: "(Oxford Dictionary)",
-                      style: TextStyle(fontStyle: FontStyle.italic),
-                    ),
-                  ],
+                style: TextStyle(
+                  fontSize: size.height * 0.02,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
               SizedBox(height: size.height / 60),
-              RichText(
-                text: TextSpan(
-                  style: TextStyle(
-                    fontSize: 0.020 * size.height,
-                    color: Theme.of(context).colorScheme.onSecondary,
-                  ),
-                  children: const [
-                    TextSpan(
-                      text: "inflation",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    TextSpan(
-                      text: "- the increase of prices over time",
-                    ),
-                  ],
-                ),
+              keyVocabulary(
+                "Investing -",
+                "putting (money) into financial schemes, shares, property, or a commercial venture with the expectation of achieving a profit (Oxford Dictionary)",
               ),
+              SizedBox(height: size.height / 50),
+              keyVocabulary("Inflation -", "the increase of prices over time"),
               SizedBox(height: size.height / 30),
               Text(
                 "Money, essentially worthless alone, gains value as a medium of exchange for goods and services. A \$10 bill, is worth \$10 dollars because we can buy with it sth that is 10 times more valuable than sth for \$1. Governments regulate the supply of money, often through printing more, but this doesn't create wealth. Increasing the money supply without a corresponding rise in production leads to inflation. This happens because more money chases the same goods and services. For instance, if the government doubles the money supply without boosting production, prices roughly double too. What are the consequences ? Our savings lose value over time, as we can buy less and less with it.",
@@ -161,18 +196,67 @@ class _Lesson1 extends State<Lesson1> {
                   color: Theme.of(context).colorScheme.onSecondary,
                 ),
               ),
-              SizedBox(height: size.height / 30),
+              SizedBox(height: size.height / 10),
               buildQuizScreen(
-                question:
-                    "Below you can see a chart showing the inflation rate in Algeria during a time period of 4 years. Let’s assume an individual holds \$100k in cash at the beginning of the year 2019. How much will his money be worth at the end of the year 2022 ?",
-                answers: [
-                  "around \$80.5k",
-                  "To lose money",
-                  "To make money",
-                  "To spend money",
-                ],
-                correctAnswer: 0,
-                image: "assets/investing/algieria.png",
+                questionNumber: 0,
+                image: "assets/investing/lesson1/algieria.png",
+              ),
+              SizedBox(height: size.height / 10),
+              Text(
+                "Alright, but while we can print money, we can't print new stocks of a company, another S&P500 ETF, or another bitcoin. This highlights that investing in assets with real worth protects against inflation. An apple remains an apple, but \$5 won't hold its value after 10 years! :)",
+                style: TextStyle(
+                  fontSize: 0.02 * size.height,
+                ),
+              ),
+              SizedBox(height: size.height / 10),
+              Text(
+                "2. Create Assets",
+                style: TextStyle(
+                  fontSize: size.width / 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: size.height / 40),
+              Text(
+                "Key vocabulary:",
+                style: TextStyle(
+                  fontSize: size.height * 0.02,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: size.height / 60),
+              keyVocabulary(
+                "Asset -",
+                "generates money for us; in order to become wealthy we need to have assets",
+              ),
+              SizedBox(height: size.height / 50),
+              keyVocabulary(
+                "Liabilities -",
+                "takes away money from us; in order to become wealthy we need to have as few liabilities as we can",
+              ),
+              SizedBox(height: size.height / 10),
+              Text(
+                "While expanding on this point, I am going to refer to the ideas presented in the book 'Rich Dad, Poor Dad' by Robert Kiyosaki, which I highly recommend reading. : ). Alright, in the book, assets are depicted as anything that puts money in your pocket. Kiyosaki emphasizes the concept of making money work for you by investing in assets (as he said, 'Make money work for you'). These could be stocks of companies, real estate properties, businesses, or other income-generating ventures. Assets are the key to financial independence because they generate passive income and increase in value over time. In simple words, you do nothing, and money flows into your pockets.",
+                style: TextStyle(
+                  fontSize: size.height * 0.02,
+                ),
+              ),
+              SizedBox(height: size.height / 60),
+              Text(
+                "So now, let’s do a few exercises:",
+                style: TextStyle(fontSize: size.height * 0.02),
+              ),
+              SizedBox(height: size.height / 10),
+              buildQuizScreen(
+                questionNumber: 1,
+              ),
+              SizedBox(height: size.height / 10),
+              buildQuizScreen(
+                questionNumber: 2,
+              ),
+              SizedBox(height: size.height / 10),
+              buildQuizScreen(
+                questionNumber: 3,
               ),
               SizedBox(height: size.height / 10),
               Center(
