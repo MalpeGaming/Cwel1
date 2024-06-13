69b84bfb47bed8d8c17aef357df62697373b6b6b
mzums <annsie035@gmail.com>
2024-06-14 00:20:02 +0200

    questions added

---
+++ lib/investing/lesson15/questions.dart
@@ -0,0 +1,113 @@
+var questions = [
+  [
+    {
+      "question":
+          "According to the table, which Treasury security has the highest yield?",
+      "answers": [
+        "3 Month",
+        "2 Year",
+        "10 Year",
+        "30 Year",
+      ],
+      "correctAnswer": 0,
+    },
+    {
+      "question":
+          "What is the yield change in basis points for the 5-year Treasury over the last month?",
+      "answers": [
+        "+9 bps",
+        "+14 bps",
+        "+24 bps",
+        "+27 bps",
+      ],
+      "correctAnswer": 3,
+    },
+    {
+      "question": "Which Treasury security has a coupon rate of 4.88%?",
+      "answers": [
+        "2 Year",
+        "5 Year",
+        "10 Year",
+        "30 Year",
+      ],
+      "correctAnswer": 0,
+    },
+    {
+      "question":
+          "If an investor buys \$10,000 worth of 10-year Treasury bonds at the current price listed, how many bonds can they purchase?",
+      "answers": [
+        "105.56 bonds",
+        "104.84 bonds",
+        "104.28 bonds",
+        "103.58 bonds",
+      ],
+      "correctAnswer": 1,
+    },
+    {
+      "question":
+          "If the yield of the 2-year Treasury is projected to increase by 5 basis points from the current rate, what will be its new yield?",
+      "answers": [
+        "4.98%",
+        "4.93%",
+        "4.85%",
+        "4.88%",
+      ],
+      "correctAnswer": 0,
+    },
+    {
+      "question":
+          "If the 30-year Treasury yield decreased by 0.10%, how much would the bond price change, assuming a direct relationship between yield and price? Assume a direct correlation between yield and price, with the price change being directly proportional to the yield change.",
+      "answers": [
+        "Decrease by approximately 2.3%",
+        "Increase by approximately 2.3%",
+        "Decrease by approximately 1.5%",
+        "Increase by approximately 1.5%",
+      ],
+      "correctAnswer": 1,
+    },
+    {
+      "question":
+          "If the coupon rate of the 5-year Treasury increased by 0.5%, and the bond price remained the same, what would the new yield be?",
+      "answers": [
+        "4.10%",
+        "4.55%",
+        "5.10%",
+        "4.73%",
+      ],
+      "correctAnswer": 2,
+    },
+    {
+      "question":
+          "Assume that the prices of the 3-month and 6-month Treasury securities are expected to converge to an average value within the next month. What would this average price be?",
+      "answers": [
+        "5.24",
+        "5.20",
+        "5.22",
+        "5.18",
+      ],
+      "correctAnswer": 1,
+    },
+    {
+      "question":
+          "If an investor holds \$50,000 in 12-month Treasuries and reinvests the proceeds into 2-year Treasuries after one year, what will be the total interest earned if the yields remain unchanged?",
+      "answers": [
+        "\$5,055",
+        "\$5,980",
+        "\$4,935",
+        "\$6,280",
+      ],
+      "correctAnswer": 0,
+    },
+    {
+      "question":
+          "Assuming an investor holds a portfolio of \$100,000 equally distributed among the 2-year, 5-year, and 10-year Treasuries, calculate the average yield of the portfolio.",
+      "answers": [
+        "4.71%",
+        "4.61%",
+        "4.94%",
+        "5.18%",
+      ],
+      "correctAnswer": 0,
+    }
+  ]
+];
---
+++ lib/investing/lesson17/questions.dart
@@ -0,0 +1,67 @@
+var questions = [
+  {
+    "question":
+        "Which type of ETF aims to provide exposure to the entire stock market or significant portions of it?",
+    "answers": [
+      "Sector ETFs",
+      "Broad Market ETFs",
+      "Bond ETFs",
+      "Commodity ETFs",
+    ],
+    "correctAnswer": 1,
+    "explanation":
+        "The answer is B) Broad Market ETFs. These ETFs aim to track broad indexes that cover a large portion of the market, such as the S&P 500.",
+  },
+  {
+    "question":
+        "What type of ETF would you choose if you wanted to invest in stocks of companies outside your home country?",
+    "answers": [
+      "Bond ETFs",
+      "Commodity ETFs",
+      "International ETFs",
+      "Thematic ETFs",
+    ],
+    "correctAnswer": 2,
+    "explanation":
+        "The answer is C) International ETFs. These ETFs invest in companies outside the investor’s home country, providing exposure to global markets.",
+  },
+  {
+    "question":
+        "If you believe a particular industry, like healthcare, is going to perform well, which type of ETF might you consider?",
+    "answers": [
+      "Broad Market ETFs",
+      "Bond ETFs",
+      "Leveraged ETFs",
+      "Sector ETFs",
+    ],
+    "correctAnswer": 3,
+    "explanation":
+        "The answer is D) Sector ETFs. These ETFs focus on specific industries or sectors, allowing investors to target areas they believe will perform well.",
+  },
+  {
+    "question":
+        "Which type of ETF is more suitable for short-term trading due to its complex strategies involving financial derivatives?",
+    "answers": [
+      "Broad Market ETFs",
+      "Inverse ETFs",
+      "Bond ETFs",
+      "International ETFs",
+    ],
+    "correctAnswer": 1,
+    "explanation":
+        "The answer is B) Inverse ETFs. These ETFs use financial derivatives to achieve the opposite returns of an underlying index and are not suitable for long-term investments due to their increased risk.",
+  },
+  {
+    "question":
+        "Why might investors choose to include Commodity ETFs in their portfolios?",
+    "answers": [
+      "To invest directly in physical commodities",
+      "To gain exposure to commodities like gold or oil without physically purchasing them",
+      "To avoid exposure to the stock market entirely",
+      "To amplify their returns using leverage",
+    ],
+    "correctAnswer": 1,
+    "explanation":
+        "The answer is B) To gain exposure to commodities like gold or oil without physically purchasing them. Commodity ETFs allow investors to participate in the commodities market without needing to handle the physical assets.",
+  },
+];
