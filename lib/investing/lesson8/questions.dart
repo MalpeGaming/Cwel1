7036aa058fd5f602df8118389eac24b36a4a5e4a
piter231 <piotrgajak02@gmail.com>
2024-06-14 09:53:27 +0200

    lesson 8 done

--- lib/investing/lesson8/questions.dart
+++ lib/investing/lesson8/questions.dart
@@ -107,5 +107,115 @@ var questions = [
       "No financial risk",
     ],
     "correctAnswer": 2,
-  }
+  },
+  {
+    "question":
+        "If a company decides to increase its dividend payout, how would this affect its dividend yield if the stock price remains constant?",
+    "answers": [
+      "The dividend yield will decrease.",
+      "The dividend yield will remain the same.",
+      "The dividend yield will increase.",
+      "The dividend yield is unaffected by dividend payouts.",
+    ],
+    "correctAnswer": 2,
+  },
+  {
+    "question":
+        "If Company A has a dividend yield of 3% and Company B has a dividend yield of 1%, what does this imply about the relative dividend income potential between the two companies, assuming all other factors are similar?",
+    "answers": [
+      "Company A provides a higher dividend income relative to its stock price.",
+      "Company B provides a higher dividend income relative to its stock price.",
+      "Company A is overvalued.",
+      "Company B has higher revenue.",
+    ],
+    "correctAnswer": 0,
+  },
+  {
+    "question":
+        "A company has a high ROA compared to others in its industry. What does this suggest about its use of assets?",
+    "answers": [
+      "The company uses its assets less effectively than its competitors.",
+      "The company has a lower profit margin than its competitors.",
+      "The company uses its assets more effectively than its competitors.",
+      "The company has less debt than its competitors.",
+    ],
+    "correctAnswer": 2,
+  },
+  {
+    "question":
+        "When considering a company with a high ROE but low profitability, what could this imply about its financial structure?",
+    "answers": [
+      "The company has a high amount of debt relative to equity.",
+      "The company has a high asset turnover rate.",
+      "The company has a low debt-to-equity ratio.",
+      "The company uses cash-based accounting.",
+    ],
+    "correctAnswer": 0,
+  },
+  {
+    "question":
+        "If a company has seen consistent growth in its quarterly revenue, but its quarterly earnings are declining, what might this indicate?",
+    "answers": [
+      "The company is cutting costs significantly.",
+      "The company is likely to be taken over.",
+      "The company’s expenses are rising faster than its revenue.",
+      "The company is reducing its liabilities.",
+    ],
+    "correctAnswer": 2,
+  },
+  {
+    "question":
+        "A company has consistently reported a high P/E ratio compared to its peers. What might this suggest about investor expectations?",
+    "answers": [
+      "Investors believe the company has high growth potential.",
+      "Investors believe the company is overvalued.",
+      "Investors believe the company has low profit margins.",
+      "Investors believe the company is financially distressed.",
+    ],
+    "correctAnswer": 0,
+  },
+  {
+    "question":
+        "A company has a low EV/EBITDA ratio compared to others in the industry. What might this suggest?",
+    "answers": [
+      "The company has a low net profit margin.",
+      "The company is undervalued relative to its earnings.",
+      "The company is overvalued relative to its earnings.",
+      "The company has high cash flow.",
+    ],
+    "correctAnswer": 1,
+  },
+  {
+    "question":
+        "If a company has a beta greater than 1.5, how is its stock expected to behave relative to the overall market?",
+    "answers": [
+      "The stock is expected to be less volatile than the market.",
+      "The stock is expected to have similar volatility to the market.",
+      "The stock is expected to be more volatile than the market.",
+      "The stock is not expected to be affected by market volatility.",
+    ],
+    "correctAnswer": 2,
+  },
+  {
+    "question":
+        "A company has a high total debt/equity ratio. What does this imply about its financial structure?",
+    "answers": [
+      "The company has high debt relative to shareholder equity.",
+      "The company is using equity financing primarily.",
+      "The company is undervalued relative to its assets.",
+      "The company has low financial leverage.",
+    ],
+    "correctAnswer": 0,
+  },
+  {
+    "question":
+        "A company's total cash per share is significantly higher than its stock price. What might this indicate?",
+    "answers": [
+      "The company is undervalued.",
+      "The company has low cash reserves.",
+      "The company is overvalued.",
+      "The company is facing liquidity issues.",
+    ],
+    "correctAnswer": 0,
+  },
 ];
