812464f757294ba4bedd463da94f44f9926e3cc7
mzums <annsie035@gmail.com>
2024-06-26 21:44:15 +0200

    trailing commas

--- lib/investing/lesson26/questions.dart
+++ lib/investing/lesson26/questions.dart
@@ -6,9 +6,9 @@ var questions = [
       "The total value of all coins in circulation",
       "The market price of one coin",
       "The total supply of a coin",
-      "The average transaction volume"
+      "The average transaction volume",
     ],
-    "correctAnswer": 0
+    "correctAnswer": 0,
   },
   {
     "question": "What does a high trading volume indicate?",
@@ -16,9 +16,9 @@ var questions = [
       "Strong market interest and liquidity",
       "Weak market interest and low liquidity",
       "High price volatility",
-      "Low price volatility"
+      "Low price volatility",
     ],
-    "correctAnswer": 0
+    "correctAnswer": 0,
   },
   {
     "question":
@@ -27,9 +27,9 @@ var questions = [
       "The total number of coins that will ever be produced",
       "The number of coins currently available in the market",
       "The number of coins burned or lost",
-      "The number of new coins issued in the last year"
+      "The number of new coins issued in the last year",
     ],
-    "correctAnswer": 1
+    "correctAnswer": 1,
   },
   {
     "question": "What does liquidity indicate in cryptocurrency trading?",
@@ -37,9 +37,9 @@ var questions = [
       "The ease of converting a cryptocurrency into cash",
       "The rate at which new coins are produced",
       "The security of a cryptocurrency's blockchain",
-      "The average transaction size in a blockchain"
+      "The average transaction size in a blockchain",
     ],
-    "correctAnswer": 0
+    "correctAnswer": 0,
   },
   {
     "question":
@@ -48,9 +48,9 @@ var questions = [
       "The current market capitalization based on circulating supply",
       "The potential market capitalization assuming all tokens are in circulation",
       "The value of new tokens to be issued in the future",
-      "The value of coins burned or lost"
+      "The value of coins burned or lost",
     ],
-    "correctAnswer": 1
+    "correctAnswer": 1,
   },
   {
     "question":
@@ -59,9 +59,9 @@ var questions = [
       "The maximum number of coins that will ever exist",
       "The number of coins currently available in the market",
       "The number of coins in circulation and locked/unavailable",
-      "The number of coins that have been burned or lost"
+      "The number of coins that have been burned or lost",
     ],
-    "correctAnswer": 2
+    "correctAnswer": 2,
   },
   {
     "question": "What does maximum supply represent in cryptocurrencies?",
@@ -69,8 +69,8 @@ var questions = [
       "The number of coins that can be issued annually",
       "The total number of coins that will ever be created",
       "The number of coins currently available in the market",
-      "The number of coins that have been mined in the last year"
+      "The number of coins that have been mined in the last year",
     ],
-    "correctAnswer": 1
+    "correctAnswer": 1,
   }
 ];
